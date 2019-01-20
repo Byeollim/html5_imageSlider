433afc64fc40bcb8e42bf932bb84eeccfa08a5d2
Byeol <dnquffla@gmail.com>
2019-01-20 16:07:43 +0900

    type2 complete

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-    // $("#slider ul li").eq(1).hide();
-    // $("#slider ul li").eq(2).hide();
+    $("#slider ul li").eq(1).hide();
+    $("#slider ul li").eq(2).hide();
 
-    $("#slider ul li:nth-child(2)").hide();
-    $("#slider ul li:nth-child(3)").hide();
+    // $("#slider ul li:nth-child(2)").hide();
+    // $("#slider ul li:nth-child(3)").hide();
 
     setInterval(function(){
-
-        $("#slider ul li:first-of-type").fadeOut();
-        $("#slider ul li:nth-child(2)").fadeIn();
-        $("#slider ul").append($("#slider ul li:first-of-type"));
+        $("#slider ul li:first-of-type").fadeOut(2000).next().fadeIn(1000).end().appendTo($("#slider ul"));
+        // $("#slider ul li:first-of-type").fadeOut(2000);
+        // $("#slider ul li:nth-child(2)").fadeIn(1000);
+        // $("#slider ul").append($("#slider ul li:first-of-type"));
         
-    }, 2000);
+    }, 3000);
     
     
 
